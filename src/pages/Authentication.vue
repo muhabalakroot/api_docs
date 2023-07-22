5ac005a3e037f0691d2eb7bce5b397dd4a68031c
Muhab Alakroot <101064491+muhabalakroot@users.noreply.github.com>
2023-07-22 17:55:05 +0200

    doen with the Auth page

--- src/pages/Authentication.vue
+++ src/pages/Authentication.vue
@@ -15,15 +15,232 @@
 
       <v-divider class="ma-2"></v-divider>
 
-      <div class="text-subtitle-1 font-weight-black">Brief description:</div>
+      <div class="text-subtitle-1 font-weight-black mt-3">
+        Brief description:
+      </div>
       <ul class="ml-5">
         <li>Used to get the token to be able to use the system</li>
       </ul>
 
-      <div class="text-subtitle-1 font-weight-black">Request URL:</div>
+      <div class="text-subtitle-1 font-weight-black mt-3">Request URL:</div>
       <ul class="ml-5">
-        <li>Used to get the token to be able to use the system</li>
+        <li>
+          <v-alert>
+            <code>https://app.vanex.ly/api/v1/authenticate</code>
+          </v-alert>
+        </li>
       </ul>
+
+      <div class="text-subtitle-1 font-weight-black mt-3">Method:</div>
+      <ul class="ml-5">
+        <li>POST</li>
+      </ul>
+
+      <div class="text-subtitle-1 font-weight-black mt-3">Request example:</div>
+      <ul class="ml-5">
+        <li>
+          <v-alert>
+            <code
+              >https://app.vanex.ly/api/v1/authenticate?email=example@gmail.com&password=test1234</code
+            >
+          </v-alert>
+        </li>
+      </ul>
+
+      <div class="text-subtitle-1 font-weight-black mt-3">Parameter:</div>
+      <v-table density="compact">
+        <thead style="background-color: lightblue">
+          <tr>
+            <th class="text-left">Parameter</th>
+            <th class="text-left">Required</th>
+            <th class="text-left">Type</th>
+            <th class="text-left">Expanation</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>email</td>
+            <td>yes</td>
+            <td>String</td>
+            <td>The user's email address.</td>
+          </tr>
+          <tr>
+            <td>password</td>
+            <td>yes</td>
+            <td>String</td>
+            <td>User password</td>
+          </tr>
+        </tbody>
+      </v-table>
+
+      <div class="text-subtitle-1 font-weight-black mt-3">Return example:</div>
+      <v-alert>
+        <code>
+          <pre>
+{
+    "status_code": 200,
+    "message": "",
+    "data": {
+        "status_code": 200,
+        "message": "تم تسجيل الدخول",
+        "access_token": "148607|dUQKq4FemUderGM7ThzmKHQrxm5uYDo489jP9hHe",
+        "token_type": "Bearer",
+        "user": {
+            "id": 10211,
+            "name": "مستخدم تجريبي",
+            "email": "user@vanex.ly",
+            "type": 1,
+            "full_access": 1,
+            "branch_region": null,
+            "storage_subscription": 1,
+            "office_cities": [
+                42,
+                200,
+                500,
+                69,
+                222,
+                501,
+                319,
+                320,
+                502
+            ],
+            "avatar": "https://app.vanex.ly/storage/users/pics/10211_WEIP6oFr7qTUgvEW.png"
+        }
+    },
+    "errors": false,
+    "code": null
+}
+          </pre>
+        </code>
+      </v-alert>
+
+      <div class="text-subtitle-1 font-weight-black mt-3">
+        Return parameter description:
+      </div>
+      <v-table density="compact">
+        <thead style="background-color: lightblue">
+          <tr>
+            <th class="text-left">Parameter</th>
+            <th class="text-left">Type</th>
+            <th class="text-left">Expanation</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>status_code</td>
+            <td>Number</td>
+            <td>
+              The status code of the response, indicating the success or failure
+              of the request.
+            </td>
+          </tr>
+          <tr>
+            <td>message</td>
+            <td>String</td>
+            <td>A general message related to the response.</td>
+          </tr>
+          <tr>
+            <td>data</td>
+            <td>String</td>
+            <td>
+              The main data section of the response, containing detailed
+              information about the request.
+            </td>
+          </tr>
+          <tr>
+            <td>access_token</td>
+            <td>String</td>
+            <td>
+              An access token associated with the user session, which can be
+              used for further authenticated requests.
+            </td>
+          </tr>
+          <tr>
+            <td>token_type</td>
+            <td>String</td>
+            <td>The type of access token provided.</td>
+          </tr>
+          <tr>
+            <td>user</td>
+            <td>String</td>
+            <td>Information about the logged-in user.</td>
+          </tr>
+          <tr>
+            <td>user.id</td>
+            <td>Number</td>
+            <td>The user's ID.</td>
+          </tr>
+          <tr>
+            <td>user.name</td>
+            <td>String</td>
+            <td>The name of the user.</td>
+          </tr>
+          <tr>
+            <td>user.email</td>
+            <td>String</td>
+            <td>The user's email address.</td>
+          </tr>
+          <tr>
+            <td>user.type</td>
+            <td>Number</td>
+            <td>The type or role of the user within the application.</td>
+          </tr>
+          <tr>
+            <td>user.full_access</td>
+            <td>Number</td>
+            <td>
+              A flag indicating whether the user has full access to the
+              application or not.
+            </td>
+          </tr>
+          <tr>
+            <td>user.branch_region</td>
+            <td>String</td>
+            <td>Information about the user's branch or region.</td>
+          </tr>
+          <tr>
+            <td>user.storage_subscription</td>
+            <td>Number</td>
+            <td>
+              A flag indicating whether the user has a storage subscription or
+              not.
+            </td>
+          </tr>
+          <tr>
+            <td>user.office_cities</td>
+            <td>Array</td>
+            <td>
+              An array containing multiple city IDs associated with the user.
+            </td>
+          </tr>
+          <tr>
+            <td>user.avatar</td>
+            <td>String</td>
+            <td>The URL to the user's avatar image.</td>
+          </tr>
+          <tr>
+            <td>errors</td>
+            <td>Boolean</td>
+            <td>
+              A boolean indicating whether there were any errors during the
+              request.
+            </td>
+          </tr>
+          <tr>
+            <td>code</td>
+            <td>String</td>
+            <td>
+              A specific error code associated with any potential errors in the
+              request.
+            </td>
+          </tr>
+        </tbody>
+      </v-table>
     </div>
   </v-card>
 </template>
+<style>
+code {
+  color: red;
+}
+</style>
--- src/pages/Preface.vue
+++ src/pages/Preface.vue
@@ -10,7 +10,7 @@
       you with a round trip courier services with cash on delivery with an
       elegant tracking application in order to optimize your business.
     </v-alert>
-    
+
     <v-divider class="ma-2"></v-divider>
 
     <div class="my-2 mt-4 text-h4">About VANEX API</div>
@@ -19,16 +19,16 @@
       application to our systems in order to migrate your orders directly once
       checked out from your system.<br />
       In addition, our API gives you the ability to track your packages directly
-      from your system via our package tracking services. <br />Our service
-      provides you with an API over HTTP POST & GET requests with parameters URL
-      encoded into the request and its response is encoded in JSON. It is
-      designed to be easy to use and you can implement it in any model computer
-      language that allows you generating web requests.<br />All of the
-      operation requires bearer token which can be easily received from the API
-      once authenticated using the same username and password used to login to
-      our system. <br />We strongly recommend to connect to our sandbox API
-      (Testing server) to generate some test requests before communication with
-      the real system.
+      from your system via our package tracking services.
+      <br />Our service provides you with an API over HTTP POST & GET requests
+      with parameters URL encoded into the request and its response is encoded
+      in JSON. It is designed to be easy to use and you can implement it in any
+      model computer language that allows you generating web requests.<br />All
+      of the operation requires bearer token which can be easily received from
+      the API once authenticated using the same username and password used to
+      login to our system. <br />We strongly recommend to connect to our sandbox
+      API (Testing server) to generate some test requests before communication
+      with the real system.
     </v-alert>
   </v-card>
 </template>
