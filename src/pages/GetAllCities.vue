9151904e83947056ee2a90e4c7f122a06a754876
Muhab Alakroot <101064491+muhabalakroot@users.noreply.github.com>
2023-07-23 18:25:32 +0200

    Get cities is done

--- src/pages/GetAllCities.vue
+++ src/pages/GetAllCities.vue
@@ -1,3 +1,210 @@
 <template>
-  <p>GetAllCities</p>
+  <v-card class="ma-4 pa-6">
+    <div class="mb-2 text-h3" align="center">Utils</div>
+
+    <v-divider class="ma-2"></v-divider>
+
+    <v-alert
+      >The Utils provides a set of useful utility endpoints and functionalities
+      that can be utilized by various parts in your application.
+    </v-alert>
+
+    <div>
+      <div class="ma-2 text-h4">Get All Cities</div>
+
+      <v-divider class="ma-2"></v-divider>
+
+      <div class="text-subtitle-1 font-weight-black mt-3">
+        Brief description:
+      </div>
+      <ul class="ml-5">
+        <li>It allows users to get all of the Cities with there IDs.</li>
+      </ul>
+
+      <div class="text-subtitle-1 font-weight-black mt-3">Request URL:</div>
+      <ul class="ml-5">
+        <li>
+          <v-alert>
+            <code>https://app.vanex.ly/api/v1/city/names</code>
+          </v-alert>
+        </li>
+      </ul>
+
+      <div class="text-subtitle-1 font-weight-black mt-3">Method:</div>
+      <ul class="ml-5">
+        <li>GET</li>
+      </ul>
+
+      <div class="text-subtitle-1 font-weight-black mt-3">Request example:</div>
+      <ul class="ml-5">
+        <li>
+          <v-alert>
+            <code>https://app.vanex.ly/api/v1/city/names</code>
+          </v-alert>
+        </li>
+      </ul>
+
+      <!-- <div class="text-subtitle-1 font-weight-black mt-3">Query Params:</div>
+      <v-table density="compact">
+        <thead style="background-color: lightblue">
+          <tr>
+            <th class="text-left">Query Params</th>
+            <th class="text-left">Required</th>
+            <th class="text-left">Type</th>
+            <th class="text-left">Expanation</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>page</td>
+            <td>no</td>
+            <td>Number</td>
+            <td>pagenation</td>
+          </tr>
+        </tbody>
+      </v-table> -->
+
+      <div class="text-subtitle-1 font-weight-black mt-3">Return example:</div>
+      <v-alert>
+        <code>
+          <pre>
+{
+    "status_code": 200,
+    "message": "قائمة أسماء المدن",
+    "data": [
+        {
+            "id": 1,
+            "name": "طرابلس",
+            "name_en": "Tripoli",
+            "code": "TIP",
+            "price": 10,
+            "branch": 1,
+            "est_time": "يوم",
+            "region": "TIP",
+            "branch_region_details": {
+                "id": 1,
+                "name": "طرابلس"
+            }
+        },
+        {
+            "id": 2,
+            "name": "بنغازي",
+            "name_en": "Benghazi",
+            "code": "BEN",
+            "price": 30,
+            "branch": 1,
+            "est_time": "ثلاثة أيام",
+            "region": "BEN",
+            "branch_region_details": {
+                "id": 1,
+                "name": "طرابلس"
+            }
+        },
+        {
+            "id": 3,
+            "name": "سبها",
+            "name_en": "Sebha",
+            "code": "SBH",
+            "price": 30,
+            "branch": 1,
+            "est_time": "من ثلاثة إلى خمسة أيام",
+            "region": "STH",
+            "branch_region_details": {
+                "id": 1,
+                "name": "طرابلس"
+            }
+        },
+    ],
+    "errors": false
+}
+          </pre>
+        </code>
+      </v-alert>
+
+      <div class="text-subtitle-1 font-weight-black mt-3">
+        Response body description:
+      </div>
+      <v-table density="compact">
+        <thead style="background-color: lightblue">
+          <tr>
+            <th class="text-left">Parameter</th>
+            <th class="text-left">Type</th>
+            <th class="text-left">Expanation</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>status_code</td>
+            <td>Number</td>
+            <td>The status code of the response.</td>
+          </tr>
+          <tr>
+            <td>message</td>
+            <td>String</td>
+            <td>A message associated with the response.</td>
+          </tr>
+          <tr>
+            <td>data</td>
+            <td>Object</td>
+            <td>Contains detailed information about the collect.</td>
+          </tr>
+          <tr>
+            <td>id</td>
+            <td>Number</td>
+            <td>ID of the city</td>
+          </tr>
+          <tr>
+            <td>name</td>
+            <td>String</td>
+            <td>Name of the city in Arabic</td>
+          </tr>
+          <tr>
+            <td>name_en</td>
+            <td>String</td>
+            <td>Name of the city in English</td>
+          </tr>
+          <tr>
+            <td>code</td>
+            <td>String</td>
+            <td>Code of the city</td>
+          </tr>
+          <tr>
+            <td>price</td>
+            <td>Number</td>
+            <td>Price of the city</td>
+          </tr>
+          <tr>
+            <td>branch</td>
+            <td>Number</td>
+            <td>Branch code</td>
+          </tr>
+          <tr>
+            <td>est_time</td>
+            <td>String</td>
+            <td>Estimated time for delivery</td>
+          </tr>
+          <tr>
+            <td>region</td>
+            <td>String</td>
+            <td>Region code</td>
+          </tr>
+          <tr>
+            <td>branch_region_details</td>
+            <td>object</td>
+            <td>Details of the branch region</td>
+          </tr>
+          <tr>
+            <td>errors</td>
+            <td>Boolean</td>
+            <td>Indicator for any errors.</td>
+          </tr>
+        </tbody>
+      </v-table>
+    </div>
+  </v-card>
 </template>
+<style>
+code {
+  color: red;
+}
+</style>
--- src/pages/GetAllSubCities.vue
+++ src/pages/GetAllSubCities.vue
@@ -1,3 +1,190 @@
 <template>
-  <p>GetAllSubCities</p>
+  <v-card class="ma-4 pa-6">
+    <div class="mb-2 text-h3" align="center">Utils</div>
+
+    <v-divider class="ma-2"></v-divider>
+
+    <v-alert
+      >The Utils provides a set of useful utility endpoints and functionalities
+      that can be utilized by various parts in your application.
+    </v-alert>
+
+    <div>
+      <div class="ma-2 text-h4">Get All SubCities</div>
+
+      <v-divider class="ma-2"></v-divider>
+
+      <div class="text-subtitle-1 font-weight-black mt-3">
+        Brief description:
+      </div>
+      <ul class="ml-5">
+        <li>
+          It allows users to get all of the SubCities of a City with there IDs.
+          (note that some Cities does not have SubCities)
+        </li>
+      </ul>
+
+      <div class="text-subtitle-1 font-weight-black mt-3">Request URL:</div>
+      <ul class="ml-5">
+        <li>
+          <v-alert>
+            <code>https://app.vanex.ly/api/v1/city/:city_id/subs</code>
+          </v-alert>
+        </li>
+      </ul>
+
+      <div class="text-subtitle-1 font-weight-black mt-3">Method:</div>
+      <ul class="ml-5">
+        <li>GET</li>
+      </ul>
+
+      <div class="text-subtitle-1 font-weight-black mt-3">Request example:</div>
+      <ul class="ml-5">
+        <li>
+          <v-alert>
+            <code>https://app.vanex.ly/api/v1/city/1/subs</code>
+          </v-alert>
+        </li>
+      </ul>
+
+      <!-- <div class="text-subtitle-1 font-weight-black mt-3">Query Params:</div>
+      <v-table density="compact">
+        <thead style="background-color: lightblue">
+          <tr>
+            <th class="text-left">Query Params</th>
+            <th class="text-left">Required</th>
+            <th class="text-left">Type</th>
+            <th class="text-left">Expanation</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>page</td>
+            <td>no</td>
+            <td>Number</td>
+            <td>pagenation</td>
+          </tr>
+        </tbody>
+      </v-table> -->
+
+      <div class="text-subtitle-1 font-weight-black mt-3">Return example:</div>
+      <v-alert>
+        <code>
+          <pre>
+{
+    "status_code": 200,
+    "message": "قائمة مناطق المدينة رقم : 1",
+    "data": {
+        "data": [
+            {
+                "id": 70,
+                "name": "السياحية",
+                "code": "TIP",
+                "name_en": "Seyahia",
+                "region": "TIP",
+                "price": 10,
+                "est_time": null
+            },
+            {
+                "id": 71,
+                "name": "حي الأندلس",
+                "code": "TIP",
+                "name_en": "Hay alandulus",
+                "region": "TIP",
+                "price": 10,
+                "est_time": null
+            },
+            {
+                "id": 73,
+                "name": "الظهرة",
+                "code": "TIP",
+                "name_en": "aldahraa",
+                "region": "TIP",
+                "price": 10,
+                "est_time": null
+            },
+        ]
+    },
+    "errors": false
+}
+          </pre>
+        </code>
+      </v-alert>
+
+      <div class="text-subtitle-1 font-weight-black mt-3">
+        Response body description:
+      </div>
+      <v-table density="compact">
+        <thead style="background-color: lightblue">
+          <tr>
+            <th class="text-left">Parameter</th>
+            <th class="text-left">Type</th>
+            <th class="text-left">Expanation</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>status_code</td>
+            <td>Number</td>
+            <td>The status code of the response.</td>
+          </tr>
+          <tr>
+            <td>message</td>
+            <td>String</td>
+            <td>A message associated with the response.</td>
+          </tr>
+          <tr>
+            <td>data</td>
+            <td>Object</td>
+            <td>Contains detailed information about the collect.</td>
+          </tr>
+          <tr>
+            <td>id</td>
+            <td>Number</td>
+            <td>ID of the city</td>
+          </tr>
+          <tr>
+            <td>name</td>
+            <td>String</td>
+            <td>Name of the city in Arabic</td>
+          </tr>
+          <tr>
+            <td>name_en</td>
+            <td>String</td>
+            <td>Name of the city in English</td>
+          </tr>
+          <tr>
+            <td>code</td>
+            <td>String</td>
+            <td>Code of the city</td>
+          </tr>
+          <tr>
+            <td>price</td>
+            <td>Number</td>
+            <td>Price of the city</td>
+          </tr>
+          <tr>
+            <td>est_time</td>
+            <td>String</td>
+            <td>Estimated time for delivery</td>
+          </tr>
+          <tr>
+            <td>region</td>
+            <td>String</td>
+            <td>Region code</td>
+          </tr>
+          <tr>
+            <td>errors</td>
+            <td>Boolean</td>
+            <td>Indicator for any errors.</td>
+          </tr>
+        </tbody>
+      </v-table>
+    </div>
+  </v-card>
 </template>
+<style>
+code {
+  color: red;
+}
+</style>
